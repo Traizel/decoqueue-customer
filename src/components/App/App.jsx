d60d0b39e257e2de6b37dd78f9b0da3b7c7cb808
Tre Olson <treolson@live.com>
2021-11-02 16:51:21 -0500

    more cleanup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,38 @@
 import { render } from "react-dom";
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 
-class App extends Component {
+function App () {
 
-    render() {
+    const [order, setOrder] = useState(' ');
+    const [firstName, setFirstName] = useState(' ');
+    const [lastName, setLastName] = useState(' ');
+
+    const login = (event) => {
+        event.preventDefault();
+        alert(`
+        ${order}
+        ${firstName}
+        ${lastName}
+            `);
+        setOrder(' ');
+        setFirstName(' ');
+        setLastName(' ');
+    }
+
+
     return (
         <div className="App">
-            <p>Hello! My name is Luke.</p>
-            <button>Click me!</button>
-            <p>I've clicked the button 0 times.</p>
+            <h1>Heat Transfer Warehouse Custom Order Tracker</h1>
+            <form>
+                <p>Order Number</p><input type="text" value={order} onChange={(e) => setOrder(e.target.value)}></input>
+                <p>First Name</p><input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)}></input>
+                <p>Last Name</p><input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}></input>
+                <br/>
+                <br/>
+                <button onClick={(e) => login(e)}>Login</button>
+            </form>
         </div>
     )
-  }
 }
 
 export default App;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,13 +1,19 @@
 import { combineReducers } from 'redux';
 
-// rootReducer is the primary reducer for our entire project
-// It bundles up all of the other reducers so our project can use them.
-// This is imported in index.js as rootSaga
+const loginMode = (state = 'login', action) => {
+    switch (action.type) {
+        case 'SET_TO_LOGIN_MODE':
+            return 'login';
+        case 'SET_TO_REGISTER_MODE':
+            return 'register';
+        default:
+            return state;
+    }
+};
 
-// Lets make a bigger object for our store, with the objects from our reducers.
-// This is what we get when we use 'state' inside of 'mapStateToProps'
+
 const rootReducer = combineReducers({
-
+    loginMode,
 });
 
 export default rootReducer;
